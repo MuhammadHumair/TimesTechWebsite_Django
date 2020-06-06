0e59b46fafd4978fe33db2adb1356704ffc6d1eb
Humair Razzaq <humair.comsian@outlook.com>
2020-06-06 21:29:15 +0500

    Table created in postgresql with the help of models and migrations.

--- TimesTechWebsite/TimesTech/models.py
+++ TimesTechWebsite/TimesTech/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class enquiry(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    contact = models.IntegerField()
+    subject = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    status = models.BooleanField()
